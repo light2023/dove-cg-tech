e4f756054f88b19e38d5624ae6e067ea07726ab5
Anugwom Chinenye Georgina <anugwomchinenye@gmail.com>
2025-05-20 09:30:26 +0100

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,32 @@
+// import database model
+const mongoose = require('mongoose'); 
+
+
+// config
+
+const connectDB = async () => {
+  try{
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+    });
+    console.log(MongoDB Connected: ${conn.connection.host});
+ } catch (error) {
+    console.error(Error: ${error.message});
+    process.exit(1);
+ }
+};
+module.exports = connectDB;
+
+
+
+// connection or logic
+
+// export the database
+
+
+
+
+
+
+
